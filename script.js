7e7e09f0685e835d9780d699ac50b5ea215095a9
Tomi Adenekan <tominekan@outlook.com>
2022-06-05 15:49:52 -0700

    Lazy coding, didn't init git until now

---
+++ script.js
@@ -0,0 +1,21 @@
+// Make sure that all inputs are filled out 
+var submitBtn = document.getElementById("submit-input");
+var nameField = document.getElementById("name-input");
+var emailField = document.getElementById("email-input");
+var messageField = document.getElementById("message-input");
+
+console.log(nameField, emailField, messageField, submitBtn);
+
+function validateFormInputs() {
+    console.log("validating")
+    if (nameField.value.trim() && emailField.value.trim() && messageField.value.trim()) {
+        submitBtn.removeAttribute("disabled");
+    } else {
+        submitBtn.setAttribute("disabled", "disabled");
+    }
+}
+nameField.addEventListener("change", validateFormInputs);
+emailField.addEventListener("change", validateFormInputs);
+messageField.addEventListener("change", validateFormInputs);
+
+console.log("everything seems to work")
