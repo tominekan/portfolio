6513b878b9695a6ee99ac1f701f1a66f592888a9
Tomi Adenekan <tominekan@outlook.com>
2022-06-07 15:53:56 -0700

    Working on website accessibility

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ var submitBtn = document.getElementById("submit-input");
 var nameField = document.getElementById("name-input");
 var emailField = document.getElementById("email-input");
 var messageField = document.getElementById("message-input");
+var formInput = document.getElementsByClassName("contact-form")[0]
 
+// Make sure that everything is filled out properly
+// Block submitting if it's not okay
 function validateFormInputs() {
-    console.log("validating")
     if (nameField.value.trim() && emailField.value.trim() && messageField.value.trim()) {
         submitBtn.removeAttribute("disabled");
+        return true;
     } else {
         submitBtn.setAttribute("disabled", "disabled");
+        return false;
     }
 }
+
+// Validate when the form is changed
 nameField.addEventListener("change", validateFormInputs);
 emailField.addEventListener("change", validateFormInputs);
 messageField.addEventListener("change", validateFormInputs);
+
+
+function sendMail() {
+    if (validateFormInputs()) {
+        let subject = "Message from " + nameField.value;
+        let email = emailField.value;
+        let message = messageField.value;
+        emaijs.init("tpSKAIqpao8K6bz2R");
+        emailjs.sendForm('contact_service', 'contact-form', 12);
+    }
+}
+
 
